fa51e263434e86d390e4f048e1b49fe4b1ab86c9
Christopher Martin <martincdev@gmail.com>
2021-04-30 10:36:18 -0500

    Updated comments and test.json

--- index.js
+++ index.js
@@ -4,29 +4,34 @@ const fs = require('fs');
 const debug = require('debug');
 let git = simpleGit(__dirname);
 
+let targetRepo = 'upload-repo';
+
 require('dotenv').config();
 
 const createAndWrite = async () => {
   debug.enable('simple-git,git:*');
 
   // TARGET_REPO example: https://github.com/{user}/{repository}.git, in this example I'm using my upload-repo repository
-  // The -n argument is required for noCheckout; this prevents the entire target repo from moving in with its garbage*.
-  // *no files/folders will be written when cloning.
+  // The -n argument is required for noCheckout; *this prevents the entire target repo from moving in with its garbage.
+  // *no files/folders will be written to the disk when cloning.
 
   // The folder that does get created from cloning will be incinerated at the end assuming you didn't break everything.
 
   // As a side note, you can clone the same repo in here.
-  // If cloning the same repo, do be sure to do a pull afterwards. Otherwise, you're gonna have a bad time... again.
+  // If cloning the same repo, do be sure to do a pull afterwards. Otherwise, you're gonna have a bad time when running this again.
   // TODO: add authentication
+  
+  // To switch target repository names, update TARGET_REPO in .env and targetRepo above
 
   await git.clone(process.env.TARGET_REPO, ['-n']);
 
-  const newPath = path.join(__dirname, 'git-test');
+  const newPath = path.join(__dirname, targetRepo);
   const newRepoPath = path.join(newPath, 'test.json');
-
+  
+  // Switch over to the target repo, if you don't then commits will be done under git-test
   git = simpleGit(newPath);
 
-  // Reset is required, otherwise other files/folders will be deleted in commit
+  // Reset is required, this forces the HEAD and the working files to be unstaged. Otherwise other files/folders will be deleted in commit
   // Originally tried chaining it, that didn't work out too well. For proof check out the billion of reverts in this repo. Oof.
   await git.reset('hard');
 
@@ -60,17 +65,16 @@ const createAndWrite = async () => {
   //   },
   // );
 
-  // ************** Cloned the same repo? Do a git pull after **************
-  // TODO: add check to see if repo is outdated if self cloning/updating
-
   await git
     .add('./test.json')
-    .commit('Testing env') // Could add some sort of counter to the commit? ex: Publish #55
-    .push('origin', 'master', ['--force']);
+    .commit('Your commit message goes here') // Could add some sort of counter to the commit? ex: Publish #55
+    .push('origin', 'master', ['--force']); 
+    // .pull();
+    // ************** Cloned the same repo? Uncomment the line above **************
 };
 
 const removeRepo = () => {
-  fs.rmdirSync('git-test', { recursive: true }, (err) => {
+  fs.rmdirSync(targetRepo, { recursive: true }, (err) => {
     if (err) {
       console.log(err);
     }
