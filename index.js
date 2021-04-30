9a98deb23ad427c067a395f282538fa600f72581
Christopher Martin <martincdev@gmail.com>
2021-04-30 11:54:53 -0500

    Moved stash

--- index.js
+++ index.js
@@ -24,7 +24,6 @@ const createAndWrite = async () => {
   // To switch target repository names, update TARGET_REPO in .env and targetRepo above
 
   await git.clone(process.env.TARGET_REPO, ['-n']);
-  await git.stash()
 
   const newPath = path.join(__dirname, targetRepo);
   const newRepoPath = path.join(newPath, 'test.json');
@@ -89,6 +88,7 @@ const removeRepo = () => {
 
   git = simpleGit(__dirname);
   await git
+    .stash()
     // .add('./*')
     // .commit('Updating self')
     // .push('origin', 'master', ['--force'])
